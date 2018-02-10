276fa7d08d13d198a0522692e41c9b67ac348969
wchoi32 <wchoi32@illinois.edu>
2018-02-09 16:57:13 -0800

    added schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,55 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chatd;
 
-USE chat;
+USE chatd; 
+
+DROP TABLE IF EXISTS usernames;
+DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS userNamesAndRooms;
+DROP TABLE IF EXISTS userNamesAndFriends;
+
+
+CREATE TABLE usernames (
+  username_id INTEGER NOT NULL,
+  username TEXT NOT NULL,
+  PRIMARY KEY (username_id)
+);
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message_id INTEGER NOT NULL,
+  message TEXT NOT NULL,
+  username_id INTEGER,
+  PRIMARY KEY (message_id),
+  FOREIGN KEY (username_id) REFERENCES usernames(username_id)
 );
 
 /* Create other tables and define schemas for them here! */
+-- CREATE TABLE usernames (
+--   username_id INTEGER NOT NULL,
+--   username TEXT NOT NULL,
+--   PRIMARY KEY (username_id)
+-- );
 
+CREATE TABLE rooms (
+  room_id INTEGER NOT NULL,
+  roomname TEXT NOT NULL,
+  PRIMARY KEY (room_id)
+);
 
+CREATE TABLE userNamesAndRooms (
+  username_id INTEGER,
+  roomname_id INTEGER,
+  FOREIGN KEY (username_id) REFERENCES usernames(username_id),
+  FOREIGN KEY (roomname_id) REFERENCES rooms(room_id)
+);
 
+CREATE TABLE userNamesAndFriends (
+  username_id INTEGER,
+  friend_id INTEGER,
+  FOREIGN KEY (username_id) REFERENCES usernames(username_id),
+  FOREIGN KEY (friend_id) REFERENCES usernames(username_id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
